89dca59a6224dac062d35f4a3b84c2f6feafd060
realebin <febrinafeb10@gmail.com>
2020-01-02 14:29:45 +0700

    dao pt.1

--- Dao/MenuDaoImpl.php
+++ Dao/MenuDaoImpl.php
@@ -1,22 +1,53 @@
 <?php
-Class KategoriDaoImpl{
-    public function getAllKategori(Kategori $kategori){
+class MenuDaoImpl{
+    public function getAllMenu(){
         $link = PDOUtility::get_koneksi();
         try{
-            $sql = "SELECT k.*, m.idMenu AS Menu_id FROM Kategori k 
-JOIN Menu m ON k.Menu_id = m.id_Menu";
-            $stmt = $link->prepare($sql);
-            $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE ,'Kategori');
-            //execute
-            $stmt->execute();
+            $sql = "SELECT * FROM Menu ORDER BY nama ASC";
+            $stmt = $link->query($sql);
+            $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE ,'Menu');
         }
-        catch(PDOException $err){
-            echo $err->getMessage();
+        catch (PDOException $err){
+            echo $err ->getMessage();
             die();
-
         }
         PDOUtility::close_koneksi($link);
         return $stmt;
     }
+    public function getOneMenu(Menu $menu){
+        $link = PDOUtility::get_koneksi();
+        try{
+            $sql = "SELECT * FROM Menu WHERE idMenu =?";
+            $stmt = $link->prepare($sql);
+            $stmt->bindValue(1,$menu->getIdMenu(),PDO::PARAM_INT);
+            $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE ,'Menu');
+            $stmt->execute();
+        }
+        catch (PDOException $err){
+            echo $err ->getMessage();
+            die();
+        }
+        PDOUtility::close_koneksi($link);
+        return $stmt;
+    }
+    public function insertMenu(Menu $menu){
+        $link = PDOUtility::get_koneksi();
+        $msg = 'gagal';
+        try{
+            $link->beginTransaction();
+            $sql = "INSERT INTO Menu ()";
+            $stmt = $link->prepare($sql);
+            $stmt->bindValue(1,$menu->getIdMenu(),PDO::PARAM_INT);
+            $stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE ,'Menu');
+            $stmt->execute();
+        }
+        catch (PDOException $err){
+            echo $err ->getMessage();
+            die();
+        }
+        PDOUtility::close_koneksi($link);
+        return $stmt;
+    }
+
 }
 ?>
