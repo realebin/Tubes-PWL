5ffee2313e1bf2cae1a14909b9287c10dc1ed67d
realebin <febrinafeb10@gmail.com>
2020-01-04 16:14:39 +0700

    4

--- index.php
+++ index.php
@@ -27,7 +27,6 @@ if (!isset($_SESSION['approved_user'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-    <title>Collapsible sidebar using Bootstrap 4</title>
 
     <!-- Bootstrap CSS CDN -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
